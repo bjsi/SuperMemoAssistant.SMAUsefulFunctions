6d4e962b492543e79bf6ad3f2cac17dbab720613
james <james@DESKTOP-JDJA6NK>
2020-09-04 18:07:59 +0100

    add invoke/begin invoke extensions

---
+++ ThreadingEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAUsefulFunctions
+{
+    public static class ThreadingEx
+    {
+        public static void BeginInvokeIfRequired(this ISynchronizeInvoke obj, Action action)
+        {
+            if (obj.InvokeRequired)
+                obj.BeginInvoke(action, new object[0]);
+            else
+                action();
+        }
+
+        public static void InvokeIfRequired(this ISynchronizeInvoke obj, Action action)
+        {
+            if (obj.InvokeRequired)
+                obj.Invoke(action, new object[0]);
+            else
+                action();
+        }
+    }
+}
